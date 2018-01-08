6cade0cb77cbabe28f98cc91321d8fabf3354f76
Sopellini <soping87@gmail.com>
2018-01-08 21:03:41 +0100

    zmiany po code review cz. 2 (dodana biblioteka Lombok i związany z tym refactoring klas pakietu model)

--- backend/src/main/java/com/insurance/InsuranceApp/model/Client.java
+++ backend/src/main/java/com/insurance/InsuranceApp/model/Client.java
@@ -1,14 +1,16 @@
 package com.insurance.InsuranceApp.model;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.sql.Date;
-import java.util.Objects;
 
 /**
  * Created by Sopel on 2017-05-31.
  */
 @Entity
+@Data
 public class Client implements Serializable{
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "clientSequence")
@@ -24,105 +26,4 @@ public class Client implements Serializable{
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "contact_id")
     private Contact contact;
-
-    public Client(){}
-
-    public Client(int clientId, String name, String name2, String surname, Date dob, Long pesel, Contact contact){
-        this.clientId = clientId;
-        this.name = name;
-        this.name2 = name2;
-        this.surname = surname;
-        this.dob = dob;
-        this.pesel = pesel;
-        this.contact = contact;
-    }
-
-    public int getClientId() {
-        return clientId;
-    }
-
-    public void setClientId(int clientId) {
-        this.clientId = clientId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName2() {
-        return name2;
-    }
-
-    public void setName2(String name2) {
-        this.name2 = name2;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public Date getDob() {
-        return dob;
-    }
-
-    public void setDob(Date dob) {
-        this.dob = dob;
-    }
-
-    public Long getPesel() {
-        return pesel;
-    }
-
-    public void setPesel(Long pesel) {
-        this.pesel = pesel;
-    }
-
-    public Contact getContact() {
-        return contact;
-    }
-
-    public void setContact(Contact contact) {
-        this.contact = contact;
-    }
-
-    @Override
-    public String toString() {
-        final StringBuffer sb = new StringBuffer("Client{");
-        sb.append("clientId=").append(clientId);
-        sb.append(", name='").append(name).append('\'');
-        sb.append(", name2='").append(name2).append('\'');
-        sb.append(", surname='").append(surname).append('\'');
-        sb.append(", dob=").append(dob);
-        sb.append(", pesel=").append(pesel);
-        sb.append(", contact=").append(contact);
-        sb.append('}');
-        return sb.toString();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Client)) return false;
-        Client client = (Client) o;
-        return getClientId() == client.getClientId() &&
-                Objects.equals(getName(), client.getName()) &&
-                Objects.equals(getName2(), client.getName2()) &&
-                Objects.equals(getSurname(), client.getSurname()) &&
-                Objects.equals(getDob(), client.getDob()) &&
-                Objects.equals(getPesel(), client.getPesel()) &&
-                Objects.equals(getContact(), client.getContact());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getClientId(), getName(), getName2(), getSurname(), getDob(), getPesel(), getContact());
-    }
 }
--- backend/src/main/java/com/insurance/InsuranceApp/model/Insurance.java
+++ backend/src/main/java/com/insurance/InsuranceApp/model/Insurance.java
@@ -1,14 +1,16 @@
 package com.insurance.InsuranceApp.model;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.sql.Date;
-import java.util.Objects;
 
 /**
  * Created by Sopel on 2017-08-13.
  */
 @Entity
+@Data
 public class Insurance implements Serializable{
 
     @Id
@@ -29,81 +31,4 @@ public class Insurance implements Serializable{
     @JoinColumn(name = "client_id")
     private Client client;
 
-
-    public Insurance(){}
-
-    public Insurance(int id, Date startDate, Date endDate, InsuranceType insuranceType) {
-        this.id = id;
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.insuranceType = insuranceType;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public Date getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(Date startDate) {
-        this.startDate = startDate;
-    }
-
-    public Date getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(Date endDate) {
-        this.endDate = endDate;
-    }
-
-
-    public InsuranceType getInsuranceType(){
-        return insuranceType;
-    }
-
-    public void setInsuranceType(InsuranceType insuranceType){
-        this.insuranceType = insuranceType;
-    }
-
-    public Client getClient() {
-        return client;
-    }
-
-    public void setClient(Client client) {
-        this.client = client;
-    }
-
-    @Override
-    public String toString() {
-        final StringBuffer sb = new StringBuffer("Insurance{");
-        sb.append("id=").append(id);
-        sb.append(", startDate=").append(startDate);
-        sb.append(", endDate=").append(endDate);
-        sb.append('}');
-        return sb.toString();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Insurance)) return false;
-        Insurance insurance = (Insurance) o;
-        return getId() == insurance.getId() &&
-                Objects.equals(getStartDate(), insurance.getStartDate()) &&
-                Objects.equals(getEndDate(), insurance.getEndDate()) &&
-                Objects.equals(getInsuranceType(), insurance.getInsuranceType()) &&
-                Objects.equals(getClient(), insurance.getClient());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId(), getStartDate(), getEndDate(), getInsuranceType(), getClient());
-    }
 }
